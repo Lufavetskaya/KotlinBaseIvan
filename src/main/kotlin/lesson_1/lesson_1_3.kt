f5b107c31662c90555cf2d4b97fa08b52814f76f
Lufavetskaya <lufavetzakh@gmail.com>
2023-03-28 15:15:08 +0300

    KB-1-3 add 3 variable, change variable and print it(little work with string)

---
+++ src/main/kotlin/lesson_1/lesson_1_3.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main(){
+    val yearFirstHumanInSpace: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Year of flight – $yearFirstHumanInSpace")
+    //println("Flight – 0${hour}:0${minute}")
+
+    hour = 10
+    minute = 55
+
+    println("Landing - ${hour}:${minute}")
+}
