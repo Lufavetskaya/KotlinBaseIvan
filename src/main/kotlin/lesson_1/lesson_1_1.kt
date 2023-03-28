ea4e125219927e3783132cba1c5399eb6fa74744
Lufavetskaya <lufavetzakh@gmail.com>
2023-03-28 13:11:06 +0300

    KB-1-2 add "workers" variable, change variable and print it

--- src/main/kotlin/lesson_1/lesson_1_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_1.kt
@@ -2,5 +2,5 @@ package lesson_1
 
 fun main(){
     var totalOfOrders: Int = 0
-    val tributeForBuyer: String = "Thank you for shopping with us, ${userName}."
+    val tributeForBuyer: String = "Thank you for shopping with us, Honey."
 }
