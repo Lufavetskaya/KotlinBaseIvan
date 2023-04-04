5c3d7f6d30c42a9b66a391076b070a16a42f5a70
Lufavetskaya <lufavetzakh@gmail.com>
2023-04-04 20:06:44 +0300

    KB-1-3 corrected line output

--- src/main/kotlin/lesson_1/lesson_1_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_3.kt
@@ -5,11 +5,12 @@ fun main(){
     var hour: Int = 9
     var minute: Int = 7
 
-    println("Year of flight – $yearFirstHumanInSpace")
-    //println("Flight – 0${hour}:0${minute}")
+    println(yearFirstHumanInSpace)
+    println(hour)
+    println(minute)
 
     hour = 10
     minute = 55
 
-    println("Landing - ${hour}:${minute}")
+    println("${hour}:${minute}")
 }
