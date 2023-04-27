fe201fc6784a38ac0f6cce4dc28120f6bd870656
Lufavetskaya <lufavetzakh@gmail.com>
2023-04-27 15:00:51 +0300

    KB-4-2 print class average for cargo1/cargo2 used comparison operators

---
+++ src/main/kotlin/lesson_4/lesson_4_2.kt
@@ -0,0 +1,15 @@
+package lesson_4
+
+const val MIN_WEIGHT_KG_CLASS_AVERAGE = 35
+const val MAX_WEIGHT_KG_CLASS_AVERAGE = 100
+const val MAX_VOLUME_CLASS_AVERAGE = 100
+
+fun main() {
+    val weightCargo1 = 20
+    val volumeCargo1 = 80
+    val weightCargo2 = 50
+    val volumeCargo2 = 100
+
+    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${MIN_WEIGHT_KG_CLASS_AVERAGE <= weightCargo1 && MAX_WEIGHT_KG_CLASS_AVERAGE >= weightCargo1 && MAX_VOLUME_CLASS_AVERAGE > volumeCargo1}")
+    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${MIN_WEIGHT_KG_CLASS_AVERAGE <= weightCargo2 && MAX_WEIGHT_KG_CLASS_AVERAGE >= weightCargo2 && MAX_VOLUME_CLASS_AVERAGE > volumeCargo2}")
+}
