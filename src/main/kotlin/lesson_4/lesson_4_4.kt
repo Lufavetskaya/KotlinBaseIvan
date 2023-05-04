41d80a6bebc8a33adaccafb1e63e0385d7457444
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-04 18:19:28 +0300

    KB-4-4 fix long expression, changed val training day, try used DRY

--- src/main/kotlin/lesson_4/lesson_4_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_4.kt
@@ -1,12 +1,16 @@
 package lesson_4
 
-const val TRAINING_LEGS = true
-const val TRAINING_ARMS = false
-const val TRAINING_BACK = true
-const val TRAINING_ABS = false
-
 fun main() {
-    val numberDayGym = 5
+    val trainingLegs = true
+    val trainingArms = false
+    val trainingBack = true
+    val trainingAbs = false
 
-    println("Упражнения для рук: ${(numberDayGym % 2 == 0) == TRAINING_ARMS}\nУпражнения для ног: ${((numberDayGym % 2 == 0) == TRAINING_LEGS)}\nУпражнение для спины: ${(numberDayGym % 2 == 0) == TRAINING_BACK}\nУпражнение для пресса: ${(numberDayGym % 2 == 0) == TRAINING_ABS}")
+    val numberDayGym = 5
+    val findParityDay = numberDayGym % 2 == 0
+
+    println("""Упражнения для рук: ${findParityDay == trainingArms}
+              |Упражнения для ног: ${findParityDay == trainingLegs}
+              |Упражнение для спины: ${findParityDay == trainingBack}
+              |Упражнение для пресса: ${findParityDay == trainingAbs}""".trimMargin())
 }
