dbfe846c5a1ca6790628f2286729f36cb91dd3da
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-05 20:17:26 +0300

    KB-5-1 fixed magic number and parameters in println

--- src/main/kotlin/lesson_5/lesson_5_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_1.kt
@@ -3,8 +3,11 @@ package lesson_5
 fun main() {
     println("Для входа, убедитесь, что вы не бот.\n1 + 0 = ")
 
+    val answerForCheckHuman = 1
     val answerForLogin = readln().toInt()
 
-    println(if(answerForLogin == 1) "Добро пожаловать!"
-    else "Доступ запрещен.")
+    println(
+        if(answerForLogin == answerForCheckHuman) "Добро пожаловать!"
+        else "Доступ запрещен."
+    )
 }
