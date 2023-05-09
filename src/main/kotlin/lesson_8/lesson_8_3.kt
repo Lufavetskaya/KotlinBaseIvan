125cd243f44b760145ff8eacafb5fe7b58bc836a
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-09 15:07:52 +0300

    KB-8-3 search ingredient in array without for, but use if(don't know how otherwise)

---
+++ src/main/kotlin/lesson_8/lesson_8_3.kt
@@ -0,0 +1,14 @@
+package lesson_8
+
+fun main() {
+    val ingredientsForCheesecake =
+        arrayOf("творожный сыр", "яйца", "сахарная пудра", "мука пшеничная", "сок лимона", "жирные сливки")
+
+    println("Какой ингредиент нужно проверить на наличие?")
+    val ingredientsUser = readln()
+    val badAnswerIndexOf = -1
+    if (ingredientsForCheesecake.indexOf(ingredientsUser) == badAnswerIndexOf) println("Такого ингредиента в рецепте нет")
+    else if (ingredientsUser == ingredientsForCheesecake[ingredientsForCheesecake.indexOf(ingredientsUser)]) {
+        println("Ингредиент $ingredientsUser в рецепте есть")
+    }
+}
