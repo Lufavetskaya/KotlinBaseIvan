c39e100105e9c6e9a0134d6d6115bdf6a03cfab2
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-09 23:56:57 +0300

    KB-5-5 fix conditions cmd+alt+l

--- src/main/kotlin/lesson_5/lesson_5_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_5.kt
@@ -9,9 +9,10 @@ fun main() {
     val userChoice1 = readln().toInt()
     val userChoice2 = readln().toInt()
 
-    val resultChoice = if ((userChoice1 == winNumber1 || userChoice2 == winNumber1) && (userChoice1 == winNumber2 || userChoice2 == winNumber2)) "Поздравляем! Вы выиграли главный приз!"
-    else if ((userChoice1 == winNumber1 || userChoice2 == winNumber1) || (userChoice1 == winNumber2 || userChoice2 == winNumber2)) "Вы выиграли утешительный приз!"
-    else "Неудача! Крутите барабан!"
+    val resultChoice =
+        if ((userChoice1 == winNumber1 || userChoice2 == winNumber1) && (userChoice1 == winNumber2 || userChoice2 == winNumber2)) "Поздравляем! Вы выиграли главный приз!"
+        else if ((userChoice1 == winNumber1 || userChoice2 == winNumber1) || (userChoice1 == winNumber2 || userChoice2 == winNumber2)) "Вы выиграли утешительный приз!"
+        else "Неудача! Крутите барабан!"
 
     println("Вы ввели $userChoice1 и $userChoice2!\n$resultChoice")
     println("Победные номера: $winNumber1, $winNumber2")
