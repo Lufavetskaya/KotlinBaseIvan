aad66478c7cf509d909d40d1493c17f6ce8de3d8
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-09 20:35:27 +0300

    KB-4-2 fix codestyle

--- src/main/kotlin/lesson_4/lesson_4_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_2.kt
@@ -10,10 +10,16 @@ fun main() {
     val weightCargo2 = 50
     val volumeCargo2 = 100
 
-    println("Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${MIN_WEIGHT_KG_CLASS_AVERAGE <= weightCargo1 &&
-                                                                                     MAX_WEIGHT_KG_CLASS_AVERAGE >= weightCargo1 && 
-                                                                                     MAX_VOLUME_CLASS_AVERAGE > volumeCargo1}")
-    println("Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${MIN_WEIGHT_KG_CLASS_AVERAGE <= weightCargo2 && 
-                                                                                     MAX_WEIGHT_KG_CLASS_AVERAGE >= weightCargo2 && 
-                                                                                     MAX_VOLUME_CLASS_AVERAGE > volumeCargo2}")
+    println(
+        "Average для груза с весом $weightCargo1 кг и объемом $volumeCargo1 л: ${
+            weightCargo1 in MIN_WEIGHT_KG_CLASS_AVERAGE..MAX_WEIGHT_KG_CLASS_AVERAGE &&
+                    MAX_VOLUME_CLASS_AVERAGE > volumeCargo1
+        }"
+    )
+    println(
+        "Average для груза с весом $weightCargo2 кг и объемом $volumeCargo2 л: ${
+            weightCargo2 in MIN_WEIGHT_KG_CLASS_AVERAGE..MAX_WEIGHT_KG_CLASS_AVERAGE &&
+                    MAX_VOLUME_CLASS_AVERAGE > volumeCargo2
+        }"
+    )
 }
