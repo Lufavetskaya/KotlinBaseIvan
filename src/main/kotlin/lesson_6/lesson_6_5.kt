8e699472a3c041922c223efa6a840708aafb6ff4
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-10 00:11:50 +0300

    KB-6-5 example generated itself

--- src/main/kotlin/lesson_6/lesson_6_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_5.kt
@@ -4,13 +4,13 @@ fun main() {
     var numberForTry = 0
     val maxTry = 3
 
-    println("Проверка на человека.\nПридумай и реши пример на сложение чисел от 1 до 9.")
+    println("Проверка на человека.\nРеши пример на сложение чисел от 1 до 9.")
 
     while (numberForTry < 3) {
-        println("Ввведи два слагаемых. У тебя ${maxTry - numberForTry++} попытки")
+        println("У тебя ${maxTry - numberForTry++} попытки")
 
-        val firstSummand = readln().toInt()
-        val secondSummand = readln().toInt()
+        val firstSummand = (1..9).random()
+        val secondSummand = (1..9).random()
         val sumTwoNumber = firstSummand + secondSummand
 
         println("А какова сумма?\n$firstSummand + $secondSummand =")
@@ -20,7 +20,7 @@ fun main() {
         if (sumTwoNumber == answerSum) {
             println("Добро пожаловать!")
             break
-        } else if (sumTwoNumber != answerSum && numberForTry == maxTry) println("Доступ запрещен")
+        } else if (numberForTry == maxTry) println("Доступ запрещен")
         else println("again!")
     }
 
