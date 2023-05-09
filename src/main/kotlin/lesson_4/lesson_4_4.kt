05e7bad5a7df26009d081e68c2861d1d6ee68cae
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-09 23:42:18 +0300

    KB-4-4 fix simple

--- src/main/kotlin/lesson_4/lesson_4_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_4.kt
@@ -1,16 +1,16 @@
 package lesson_4
 
 fun main() {
-    val trainingLegs = true
-    val trainingArms = false
-    val trainingBack = true
-    val trainingAbs = false
-
     val numberDayGym = 5
     val findParityDay = numberDayGym % 2 == 0
 
-    println("""Упражнения для рук: ${findParityDay == trainingArms}
-              |Упражнения для ног: ${findParityDay == trainingLegs}
-              |Упражнение для спины: ${findParityDay == trainingBack}
-              |Упражнение для пресса: ${findParityDay == trainingAbs}""".trimMargin())
+    val trainingLegs = findParityDay
+    val trainingArms = !findParityDay
+    val trainingBack = findParityDay
+    val trainingAbs = !findParityDay
+
+    println("""Упражнения для рук: $trainingArms
+              |Упражнения для ног: $trainingLegs
+              |Упражнение для спины: $trainingBack
+              |Упражнение для пресса: $trainingAbs""".trimMargin())
 }
