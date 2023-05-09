6c4f1e662d481a2e932cf271d13b6b01115e9826
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-09 15:21:20 +0300

    KB-7-3 codestyle

--- src/main/kotlin/lesson_7/lesson_7_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_3.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите последнее часло диапозона для ваших четных чисел:")
     val userNumber = readln().toInt()
 
-    for (i in 0..userNumber step 2){
+    for (i in 0..userNumber step 2) {
         println(i)
     }
 }
