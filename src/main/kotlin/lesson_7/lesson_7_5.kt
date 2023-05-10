8e087abffeb727df9803b8562a38a4feda68b612
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-10 23:07:28 +0300

    KB-7-5 micro fix string random

--- src/main/kotlin/lesson_7/lesson_7_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_5.kt
@@ -4,16 +4,8 @@ fun main() {
     println("Укажите длину пароля")
     val lengthPassUser = readln().toInt()
     var password = ""
-    var allSymbolsForPass = ""
 
-    val randomNum = 0..9
-    for (i in randomNum) allSymbolsForPass += i
-
-    val randomLetterSmall = 'a'..'z'
-    for (i in randomLetterSmall) allSymbolsForPass += i
-
-    val randomLetterBig = 'A'..'Z'
-    for (i in randomLetterBig) allSymbolsForPass += i
+    val allSymbolsForPass = ('a'..'z') + ('A'..'Z') + (0..9)
 
     for (i in 1..lengthPassUser) {
         password += allSymbolsForPass.random()
