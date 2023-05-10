540e28bd40585f44b16e08052c3a625688910b0b
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-10 23:02:12 +0300

    KB-8-5 use beautiful initialization

--- src/main/kotlin/lesson_8/lesson_8_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_5.kt
@@ -2,16 +2,10 @@ package lesson_8
 
 fun main() {
     println("Сколько ингредиентов в вашем рецепте?")
-
     val userTotalIngredients = readln().toInt()
-    val arrayIngredients = Array(userTotalIngredients) { "" }
 
     println("Введите $userTotalIngredients ингредиентов, каждый на новой строке.")
-
-    for (i in arrayIngredients.indices) {
-        val inputUserIngr = readln()
-        arrayIngredients[i] = inputUserIngr
-    }
+    val arrayIngredients = Array(userTotalIngredients) { readln() }
 
     println("Список для вашего рецепта:")
     arrayIngredients.forEach { println(it) }
