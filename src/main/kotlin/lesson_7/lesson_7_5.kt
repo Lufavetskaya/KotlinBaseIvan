f9d47aea8491764dffd6e8718de7bc26d87b910c
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-10 09:11:10 +0300

    KB-7-5 refining

--- src/main/kotlin/lesson_7/lesson_7_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_5.kt
@@ -4,16 +4,19 @@ fun main() {
     println("Укажите длину пароля")
     val lengthPassUser = readln().toInt()
     var password = ""
+    var allSymbolsForPass = ""
+
+    val randomNum = 0..9
+    for (i in randomNum) allSymbolsForPass += i
+
+    val randomLetterSmall = 'a'..'z'
+    for (i in randomLetterSmall) allSymbolsForPass += i
+
+    val randomLetterBig = 'A'..'Z'
+    for (i in randomLetterBig) allSymbolsForPass += i
 
     for (i in 1..lengthPassUser) {
-        val randomNum = (0..9).random().toString()
-        val randomLetterSmall = ('a'..'z').random().toString()
-        val randomLetterBig = ('A'..'Z').random().toString()
-        when ((1..3).random()) {
-            1 -> password += randomLetterBig
-            2 -> password += randomNum
-            3 -> password += randomLetterSmall
-        }
+        password += allSymbolsForPass.random()
     }
 
     println(password)
