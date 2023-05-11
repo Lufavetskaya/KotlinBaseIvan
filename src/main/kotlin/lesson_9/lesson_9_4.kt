e7ec069ae91ad295ecfded8a7b2dd08452df0235
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-11 15:56:52 +0300

    KB-9-4 readln to list, print sorted list

---
+++ src/main/kotlin/lesson_9/lesson_9_4.kt
@@ -0,0 +1,11 @@
+package lesson_9
+
+fun main() {
+    println("Введите 5 продуктов через запятую")
+    val productList = readln().split(", ").toMutableList()
+    productList.sort()
+
+    productList.forEach {
+        println(it)
+    }
+}
