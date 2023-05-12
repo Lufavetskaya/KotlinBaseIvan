e0bb84b8f99844454e3fa40c4304626ecd4fc6fc
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-12 22:41:38 +0300

    KB-9-4 use sorted

--- src/main/kotlin/lesson_9/lesson_9_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_4.kt
@@ -3,8 +3,7 @@ package lesson_9
 fun main() {
     println("Введите 5 продуктов через запятую")
 
-    val productList = readln().split(", ").toMutableList()
-    productList.sort()
+    val productList = readln().split(", ").toMutableList().sorted()
 
     productList.forEach {
         println(it)
