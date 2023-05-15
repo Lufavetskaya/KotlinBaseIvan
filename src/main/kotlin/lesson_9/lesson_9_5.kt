9e0a2efae1f8e02abd58ac3b6ce321e8cc89ce35
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-15 12:48:05 +0300

    KB-9-5 dokumek with beautiful code, use when

--- src/main/kotlin/lesson_9/lesson_9_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_5.kt
@@ -2,20 +2,19 @@ package lesson_9
 
 fun main() {
     val productSet = MutableList(5) { readln() }.toSet().sorted()
-    var countForPrint = 1
 
     for (i in productSet) {
-        if (countForPrint == 1) {
-            print("${i.replaceFirstChar { it.uppercase() }}, ")
-            countForPrint++
-        } else if (countForPrint == productSet.size) {
-            i.lowercase()
-            print("${i.replaceFirstChar { it.lowercase() }}.")
-            countForPrint++
-        } else {
-            i.lowercase()
-            print("${i.replaceFirstChar { it.lowercase() }}, ")
-            countForPrint++
+        when {
+            (i == productSet.first()) -> {
+                print("${i.replaceFirstChar { it.uppercase() }}, ")
+            }
+
+            (i == productSet.last()) -> {
+                print("${i.replaceFirstChar { it.lowercase() }}.")
+            }
+
+            else -> print("${i.replaceFirstChar { it.lowercase() }}, ")
+
         }
     }
 }
