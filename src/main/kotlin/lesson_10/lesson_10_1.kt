f8c44afea77b086ff2f145388c225725cef35b57
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-16 10:10:23 +0300

    KB-10-1 study pair and triple for overall development, more beautiful implementation with roll in one fun in 10-4 made

--- src/main/kotlin/lesson_10/lesson_10_1.kt
+++ src/main/kotlin/lesson_10/lesson_10_1.kt
@@ -1,21 +1,24 @@
 package lesson_10
 
 fun main() {
-    val firstRollHuman = rollDice()
-    val secondRollHuman = rollDice()
+    val firstRollHuman = rollDice().first
+    val secondRollHuman = rollDice().second
 
     println("Ход человечества: первый куб - $firstRollHuman, второй - $secondRollHuman")
 
-    val firstRollAI = rollDice()
-    val secondRollAI = rollDice()
+    val firstRollAI = rollDice().first
+    val secondRollAI = rollDice().second
 
     println("Ход ai: первый куб - $firstRollAI, второй - $secondRollAI")
 
     resultSumRoll(sumRoll(firstRollHuman, secondRollHuman), sumRoll(firstRollAI, secondRollAI))
 }
 
-fun rollDice(): Int {
-    return (1..6).random()
+fun rollDice(): Pair<Int, Int> {
+    val firstRoll = (1..6).random()
+    val secondRoll = (1..6).random()
+
+    return Pair(firstRoll, secondRoll)
 }
 
 fun sumRoll (firstR: Int, secondR: Int) : Int{
