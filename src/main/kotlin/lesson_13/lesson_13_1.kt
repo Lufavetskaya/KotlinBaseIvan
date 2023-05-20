dbe65c5425c544665258feb942e25a501b6415b6
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-20 23:48:28 +0300

    KB-13-1 create class with nullable type val

---
+++ src/main/kotlin/lesson_13/lesson_13_1.kt
@@ -0,0 +1,10 @@
+package lesson_13
+
+fun main() {
+    var annPerson = PhoneContact("Ann", 576_70_07, null)
+
+}
+
+class PhoneContact(val namePerson: String, val phonePerson: Int, val companyPerson: String?) {
+
+}
