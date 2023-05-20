9f99e4b32045d9a6f03826a538e64b7ea5c956eb
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-21 00:00:29 +0300

    KB-13-2 create class with fun print(use elvis)

---
+++ src/main/kotlin/lesson_13/lesson_13_2.kt
@@ -0,0 +1,17 @@
+package lesson_13
+
+fun main() {
+    var annPerson = PhoneContactTask2("Ann", 576_70_07, null)
+
+}
+
+class PhoneContactTask2(val namePerson: String, val phonePerson: Int, val companyPerson: String?) {
+
+    init {
+        printInfo()
+    }
+
+    fun printInfo(){
+        println("Имя: $namePerson\nНомер: $phonePerson\nКомпания: ${companyPerson ?: "[не указано]"}")
+    }
+}
