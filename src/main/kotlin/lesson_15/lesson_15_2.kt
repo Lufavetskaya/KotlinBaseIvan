8db99fad4429fa43c403f9773c2c1b0542fcb0dd
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-24 22:26:10 +0300

    KB-15-2 create abstract class for connect and send message to server, test it

---
+++ src/main/kotlin/lesson_15/lesson_15_2.kt
@@ -0,0 +1,39 @@
+package lesson_15
+
+fun main(){
+
+    val dataTemperature = DataTemperature("Server")
+    dataTemperature.sendData()
+
+    val dataRain = DataRain("Server")
+    dataRain.sendData()
+
+}
+
+abstract class UnderlyingMessage(
+    val nameServer: String
+){
+    fun connectToServer(nameServer: String){
+        println("Подключились к серверу $nameServer.")
+    }
+
+    open fun sendData(){
+        println("Отправили данные на сервер $nameServer.")
+    }
+
+}
+
+class DataTemperature(nameServer: String) : UnderlyingMessage(nameServer){
+    override fun sendData() {
+        super.connectToServer(nameServer)
+        println("Отправили данные о температуре на сервер $nameServer.")
+    }
+
+}
+
+class DataRain(nameServer: String) : UnderlyingMessage(nameServer) {
+    override fun sendData() {
+        super.connectToServer(nameServer)
+        println("Отправили данные о дожде на сервер $nameServer.")
+    }
+}
