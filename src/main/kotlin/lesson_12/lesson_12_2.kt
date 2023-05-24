0a9320bdc061cee3dfeb10a8b35cbe0af6a416f4
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-24 13:33:36 +0300

    KB-12-2 microfix alt cmd L, change var -> val

--- src/main/kotlin/lesson_12/lesson_12_2.kt
+++ src/main/kotlin/lesson_12/lesson_12_2.kt
@@ -1,6 +1,6 @@
 package lesson_12
 
-fun main(){
+fun main() {
     val thirdDayLife = KeepWeatherTask2(12, 10, true, 660)
     val fourthDayLife = KeepWeatherTask2(0, -50, false, 550)
     val fifthDayLife = KeepWeatherTask2(0, 50, true, 550)
@@ -9,19 +9,21 @@ fun main(){
     fourthDayLife.printWeather()
     fifthDayLife.printWeather()
 }
+
 class KeepWeatherTask2(_temperatureDay: Int, _temperatureNight: Int, _rain: Boolean, _atmosphericPressure: Int) {
 
-    var temperatureDay = _temperatureDay
-    var temperatureNight = _temperatureNight
-    var rain = _rain
-    var atmosphericPressure = _atmosphericPressure
+    val temperatureDay = _temperatureDay
+    val temperatureNight = _temperatureNight
+    val rain = _rain
+    val atmosphericPressure = _atmosphericPressure
 
-    fun printWeather(){
-        println("""Температура днем: $temperatureDay
+    fun printWeather() {
+        println(
+            """Температура днем: $temperatureDay
              |Температура ночь: $temperatureNight
              |Налчиие дождя: $rain
              |Атмосферное давление: $atmosphericPressure
-         """.trimMargin())
+         """.trimMargin()
+        )
     }
-
 }
