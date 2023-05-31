0147ed3aa67243918d8b0d92321864482f33d5d3
Lufavetskaya <lufavetzakh@gmail.com>
2023-05-31 18:43:56 +0300

    KB-14-4 fix name class Satellite and nameGalaxy

--- src/main/kotlin/lesson_14/lesson_14_4.kt
+++ src/main/kotlin/lesson_14/lesson_14_4.kt
@@ -1,9 +1,9 @@
 package lesson_14
 
 fun main() {
-    val firstSatellites = Satellites("Moon", "Milky Way")
+    val firstSatellite = Satellite("Moon", "Milky Way")
     val firstPlanet = Planet("Earth", "Milky Way", true, true, true, true)
-    firstPlanet.addSatellites(firstSatellites)
+    firstPlanet.addSatellites(firstSatellite)
     firstPlanet.printInfo()
 }
 
@@ -19,13 +19,13 @@ open class CelestialBody(
 
 open class Planet(
     name: String,
-    nameSolarSystem: String,
+    nameGalaxy: String,
     val inhabited: Boolean = false,
     val atmosphere: Boolean = false,
     val haveWater: Boolean = false,
     val canLanded: Boolean = false,
-) : CelestialBody(name, nameSolarSystem) {
-    val listSatellites = mutableListOf<Satellites>()
+) : CelestialBody(name, nameGalaxy) {
+    val listSatellites = mutableListOf<Satellite>()
 
     override fun printInfo() {
         println("Название небесного планеты: $name")
@@ -35,7 +35,7 @@ open class Planet(
         println("На планете есть вода: $haveWater")
         println("Возможна высадка: $canLanded")
         if (listSatellites.isNotEmpty()) {
-            print("Спутнкии планеты: ")
+            print("Спутники планеты: ")
             listSatellites.forEach {
                 println(it.name)
             }
@@ -43,9 +43,9 @@ open class Planet(
 
     }
 
-    fun addSatellites(satellites: Satellites) {
+    fun addSatellites(satellites: Satellite) {
         listSatellites.add(satellites)
     }
 }
 
-class Satellites(name: String, nameSolarSystem: String) : Planet(name, nameSolarSystem)
+class Satellite (name: String, nameGalaxy: String) : CelestialBody(name, nameGalaxy)
