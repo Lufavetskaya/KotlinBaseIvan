516d69f7b59d511901498f262a6f57a4f1e4c736
Lufavetskaya <lufavetzakh@gmail.com>
2023-06-03 11:34:40 +0300

    KB-16-2 fix val PI and use sqrt

--- src/main/kotlin/lesson_16/lesson_16_2.kt
+++ src/main/kotlin/lesson_16/lesson_16_2.kt
@@ -1,21 +1,22 @@
 package lesson_16
 
+import kotlin.math.sqrt
+
+const val PI = 3.14
 
 fun main() {
-    val firstCircle = Circle(25.0)
+    val firstCircle = Circle(2.0)
     println("Площадь круга ${firstCircle.areaOfCircle()}")
     println("Длина окружности ${firstCircle.circumference()}")
 
 }
 
 class Circle(private val radius: Double) {
-    private val PI = 3.14
-
     fun circumference(): Double {
         return 2 * PI * radius
     }
 
     fun areaOfCircle(): Double {
-        return PI * radius * radius
+        return PI * sqrt(radius)
     }
 }
