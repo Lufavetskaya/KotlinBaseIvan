c153cbfe8aa07f720c96ab4f099d8072febf273c
Lufavetskaya <lufavetzakh@gmail.com>
2023-06-04 17:39:36 +0300

    KB-17-5 create class User with login and pass, try change login and pass use set, new get for pass

---
+++ src/main/kotlin/lesson_17/lesson_17_5.kt
@@ -0,0 +1,37 @@
+package lesson_17
+
+fun main() {
+    val firstUser = User("Krosh", "12345")
+
+    println("Логин: ${firstUser.loginUser}\nПароль: ${firstUser.passwordUser}")
+
+    firstUser.loginUser = "KroshGavrosh"
+    firstUser.passwordUser = "0123"
+
+    println("Логин: ${firstUser.loginUser}\nПароль: ${firstUser.passwordUser}")
+
+}
+
+class User(
+    private val initLoginUser: String,
+    private val initPasswordUser: String,
+) {
+    var loginUser: String = initLoginUser
+        set(value) {
+            field = value
+            println("Логин успешно изменен")
+        }
+
+    var passwordUser: String = initPasswordUser
+        set(value) {
+            if (value != field) println("Вы не можете изменить пароль")
+        }
+        get() {
+            var secretPassword: String = ""
+            repeat(initPasswordUser.length) {
+                secretPassword += "*"
+            }
+            return secretPassword
+        }
+
+}
