ba30042e43d9b1d989b46f6b2ca730c849f5de48
Lufavetskaya <lufavetzakh@gmail.com>
2023-06-04 16:46:17 +0300

    KB-17-3 create class package and add secret flag

---
+++ src/main/kotlin/lesson_17/lesson_17_3.kt
@@ -0,0 +1,21 @@
+package lesson_17
+
+fun main(){
+    val firstPackage = Package("Папка 1 ", 100, true)
+    println("Имя папки: ${firstPackage.namePackage}\nКоличетсво файлов: ${firstPackage.qtFile}")
+
+}
+
+class Package(
+    private val initNamePackage: String,
+    private val initQtFile: Int,
+    val defaultSecretFlag: Boolean = false,
+){
+    var namePackage: String = initNamePackage
+        get() = if (defaultSecretFlag == true) "скрытая папка" else field
+
+    var qtFile: Int = initQtFile
+        get() = if (defaultSecretFlag == true) 0 else field
+
+
+}
