4400fe4a83c280168b12d7791c12a9279ec5b635
Lufavetskaya <lufavetzakh@gmail.com>
2023-06-09 15:20:19 +0300

    KB-18-1 try used polymorphism for Orders

---
+++ src/main/kotlin/lesson_18/lesson_18_1.kt
@@ -0,0 +1,45 @@
+package lesson_18
+
+
+fun main() {
+    val firstOrder: Order = SmallOrder(1, "tea")
+    val secondOrder: Order = BigOrder(2, listOf("tea", "milk", "potato"))
+
+    val arrayOrders = arrayOf(firstOrder, secondOrder)
+
+    fun printInfoAllOrders(arrayOrders: Array<Order>) {
+        arrayOrders.forEach { it.infoOrder() }
+    }
+
+    printInfoAllOrders(arrayOrders)
+}
+
+open class Order() {
+    open fun infoOrder() {}
+}
+
+class SmallOrder(
+    private val numberOrder: Int,
+    private val contentOrder: String,
+) : Order() {
+    override fun infoOrder() {
+        println("Номер заказа: $numberOrder\nЗаказан товар: $contentOrder")
+    }
+}
+
+class BigOrder(
+    private val numberOrder: Int,
+    private val contentOrder: List<String>,
+) : Order() {
+    override fun infoOrder() {
+        print("Номер заказа: $numberOrder\nЗаказаны следующие товары: ")
+        contentOrder.forEach {
+            if (contentOrder.indexOf(it) != contentOrder.lastIndex) {
+                print("$it, ")
+            } else {
+                print(it)
+            }
+        }
+    }
+}
+
