f89e4713d5dce4d92fca08f9676fa98974f52af3
Lufavetskaya <lufavetzakh@gmail.com>
2023-06-16 00:47:04 +0300

    KB-19-3 use todo

---
+++ src/main/kotlin/lesson_19/lesson_19_3.kt
@@ -0,0 +1,22 @@
+package lesson_19
+
+fun main() {
+    val firstShip = Spacecraft()
+
+    firstShip.offAsteroid()
+
+
+}
+
+class Spacecraft {
+    fun takeOff() {
+        // TODO здесь нужна информация о дополнительной логике
+    }
+
+    fun land() {}
+
+    fun offAsteroid() {
+        TODO()
+    }
+}
+
